a136f654a92b782d9a99a14395af2d36aeef5b5d
MohammadSiam <siamk417@gmail.com>
2024-05-22 17:48:43 +0600

    inventory test initialize

--- src/modules/inventory/tests/inventory.controller.spec.ts
+++ src/modules/inventory/tests/inventory.controller.spec.ts
@@ -2,6 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InventoryController } from '../controller/inventory.controller';
 import { InventoryService } from '../services/inventory.service';
 
+const mocInventory = {
+  intProductId: 1,
+  intQuantity: 10,
+  intInventoryId: 3,
+  dteCreatedAt: "2024-05-22T11:43:31.000Z",
+  dteUpdatedAt: "2024-05-22T11:43:31.000Z"
+}
+
 describe('InventoryController', () => {
   let controller: InventoryController;
 
